5ca761115516d7aa891504eb78eda19169c6f4a1
Sonja Parsell <hello@sparsell.com>
2020-04-24 17:45:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -112,9 +112,9 @@ def play
     turn
   end
 
-  if won?(board)
-  puts "Congratulations #{winner(board)}!"
-elsif draw?(board)
+  if won?
+  puts "Congratulations #{winner}!"
+elsif draw?
   puts "Cat's Game!"
   end
 end
