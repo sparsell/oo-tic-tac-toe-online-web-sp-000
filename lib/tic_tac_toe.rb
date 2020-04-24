d57271f3959263467f98cdd51783c553e29cdf2f
Sonja Parsell <hello@sparsell.com>
2020-04-24 17:20:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,4 +39,14 @@ class TicTacToe
   index.between?(0, 8) && !position_taken?(index)
   end
 
+  def turn_count(board)
+  number_of_turns = 0
+  board.each do |played|
+    if played == "X" || played == "O"
+    number_of_turns += 1
+    end
+    end
+    return number_of_turns
+  end
+
 end
