be1d3e0779507b54eb205aa93cd6835e266d1cd7
Sonja Parsell <hello@sparsell.com>
2020-04-24 17:23:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,4 +57,17 @@ class TicTacToe
     end
   end
 
+  def turn
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if  valid_move?(index)
+    player_token = current_player
+    move(index, token)
+    display_board
+  else
+    turn(board)
+  end
+end
+
 end
